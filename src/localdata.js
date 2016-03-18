0a53733fa436cc98f25e87d29d2de363a945a899
Nelson Taveras <ntaveras03@gmail.com>
2016-03-18 05:56:00 -0400

    Main lib api

--- src/localdata.js
+++ src/localdata.js
@@ -1,12 +1,26 @@
 'use strict';
 
+import fs from 'fs';
+import path from 'path';
+import Database from './db';
+
 class LocalData {
   constructor() {
-    this.data = 'localdata';
+    this._dir = '.LOCALDATA'; // Prefix use for directory
+    // Initialize hidden directory that will hold the data
+    try {
+      fs.statSync(this._getPath());
+    } catch (error) {
+      // Folder doesn't exist, create it
+      fs.mkdirSync(this._getPath());
+    }
   }
-  db() {
+  db(name) {
+    const db = new Database(name, this._getPath());
+    return db;
   }
-  collections() {
+  _getPath() {
+    return path.resolve(__dirname, this._dir);
   }
 }
 
